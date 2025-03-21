23324ada44ee563f54abc7780fd51b876fefd123
Imran Rashid <irashid@cloudera.com>
2025-03-20 23:10:11 -0500

    rudimentary way to move point around

--- game.py
+++ game.py
@@ -4,19 +4,48 @@ x = 0
 y = 0
 
 import pygame
+pygame.init()
+max_width = 1280
+max_height = 720
+half_width = max_width / 2
+half_height = max_height / 2
+screen = pygame.display.set_mode((max_width, max_height))
+
+
+def draw_vertical(x):
+  thickness = 10 if x == half_width else 2 
+  pygame.draw.line(screen, (0, 0, 0), (x, 0), (x,max_height), thickness)
+  
+def draw_horizontal(y):
+  thickness = 10 if y == half_height else 2
+  pygame.draw.line(screen, (0, 0, 0), (0, y), (max_width,y), thickness)
+
+def render():
+  # fill the screen with a color to wipe away anything from last frame
+  print(globals())
+  print(globals()["x"])
+  global x
+  global y
+  screen.fill("white")
+
+  # RENDER YOUR GAME HERE
+  for x_pos in range(160, 1280, 160):
+    draw_vertical(x_pos)
+  for y_pos in range(90, 720, 90):
+    draw_horizontal(y_pos)
+  
+  draw_x = (x + 4.0) * 1280 / 8
+  draw_y = (y + 4.0) * 720 / 8
+  print(f"{x}, {y}; {draw_x}, {draw_y}")
+  pygame.draw.circle(screen, (0, 0, 255), (draw_x, draw_y), 20, 20)
+
+  # flip() the display to put your work on screen
+  pygame.display.flip()
 
 
 def main():
-  pygame.init()
-  screen = pygame.display.set_mode((1280, 720))
-  evt_flag = threading.Event()
   clock = pygame.time.Clock()
 
-  def draw_vertical(x):
-    pygame.draw.line(screen, (0, 0, 0), (x, 0), (x,780), 10)
-    
-  def draw_horizontal(y):
-    pygame.draw.line(screen, (0, 0, 0), (0, y), (1280,y), 10)
 
   running = True
   while running:
@@ -26,23 +55,8 @@ def main():
           if event.type == pygame.QUIT:
               print("got a quit")
               running = False
+      render()
   
-      # fill the screen with a color to wipe away anything from last frame
-      screen.fill("white")
-  
-      # RENDER YOUR GAME HERE
-      for x_pos in range(160, 1280, 160):
-        draw_vertical(x_pos)
-      for y_pos in range(90, 720, 90):
-        draw_horizontal(y_pos)
-      
-      draw_x = (x + 4.0) * 1280 / 8
-      draw_y = (y + 4.0) * 720 / 8
-      pygame.draw.circle(screen, (0, 0, 255), (draw_x, draw_y), 20, 20)
-   
-  
-      # flip() the display to put your work on screen
-      pygame.display.flip()
   
       clock.tick(60)  # limits FPS to 60
 
@@ -51,9 +65,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-else:
-  import threading
-  t1 = threading.Thread(target=main())
-  t1.start()
-  t1.join()
-
